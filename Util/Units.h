9bbfea532e7c0ab34f0f034f9b7032c0ea2ad450
robbywalker <robby.walker@gmail.com>
2010-08-14 22:26:39 -0700

    Add Apache license

--- Util/Units.h
+++ Util/Units.h
@@ -1,11 +1,21 @@
-/*
- *  Units.h
- *  FunRun
- *
- *  Created by Robert Walker on 7/13/08.
- *  Copyright 2008 Fitnio. All rights reserved.
- *
- */
+//
+//  Units.h
+//  iphone-utils
+//
+//  Copyright 2008 The iphone-utils Authors. All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS-IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 
 #define HOUR_IN_MINUTES 60
 #define HOUR_IN_SECONDS (HOUR_IN_MINUTES * MINUTE_IN_SECONDS)
